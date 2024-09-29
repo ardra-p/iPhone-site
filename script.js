3ad575ac2ee1fba96fc9494fd56ea196ee53076b
Ardra p <165270747+ardra-p@users.noreply.github.com>
2024-09-29 18:56:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const Brand= document.querySelector(".Brand"),
+searchBtn = Brand.querySelector(".search-btn");
+
+
+searchBtn.addEventListener("click", ()=>{
+    Brand.classList.toggle("active");
+})
+
+//vbgzxdgcdehkxi//
+
+const Bran= document.querySelector(".Bran"),
+searchBt = Bran.querySelector(".search-btn");
+
+
+searchBt.addEventListener("click", ()=>{
+    Bran.classList.toggle("active");
+})
+
+//nhvcjzxhdsc,jhf//
+
+const Bra= document.querySelector(".Bra"),
+searchB = Bra.querySelector(".search-btn");
+
+
+searchB.addEventListener("click", ()=>{
+    Bra.classList.toggle("active");
+})
+
+//fhdnhskvkjnf//
+const Br= document.querySelector(".Br"),
+search = Br.querySelector(".search-btn");
+
+
+search.addEventListener("click", ()=>{
+    Br.classList.toggle("active");
+})
+ 
+//dhcxckbv,jdsnh// 
+
+const B= document.querySelector(".B"),
+searc = B.querySelector(".search-btn");
+
+
+searc.addEventListener("click", ()=>{
+    B.classList.toggle("active");
+})
+
+const blast= document.querySelector(".blast"),
+sear = blast.querySelector(".search-btn");
+
+
+sear.addEventListener("click", ()=>{
+    blast.classList.toggle("active");
+})
